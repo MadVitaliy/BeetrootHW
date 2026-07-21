3a61db944aa1525deff042b6cd3637786469a7f6
MadSkipper <omelchenko.vitaliy1@gmail.com>
2026-07-21 11:13:35 +0300

    Revert "HW 3.5 has been used as templat"
    This reverts commit 0f2bff6f14fa986fdd169c0377a0d9b0eb2cd44b.

--- module_3_6/main/Utils.h
+++
@@ -1,9 +0,0 @@
-
-namespace Utils
-{
-    template <typename TIn, typename TOut>
-    TOut map(TIn in, TIn old_min, TIn old_max, TOut new_min, TOut new_max)
-    {
-        return static_cast<TOut>(in * (new_max - new_min) / (old_max - old_min));
-    }
-}
